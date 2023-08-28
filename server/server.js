9fad0e9438eedab325eccda3430af26cff703590
Rene Malingre <rene.malingre@gmail.com>
2023-08-28 11:57:56 +0930

    Heroku postbuild script added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-// const helmet = require('helmet')
 const path = require('path')
 const { ApolloServer } = require('apollo-server-express')
 const { authMiddleware } = require('./utils/auth')
